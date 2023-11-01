62fa226d784e6d3572f21cd5686488c718edcd3a
Victor Sanchez <vickoss9004@gmail.com>
2023-11-01 13:08:23 -0600

    Avance

--- js/carrusel.js
+++ js/carrusel.js
@@ -1,9 +1,13 @@
 function inicializarCarruseles() {
-    var carousel1 = new bootstrap.Carousel(document.getElementById('carouselExampleFade1'), {
+    var carousel1 = new bootstrap.Carousel(document.getElementById('carouselExampleFade'), {
         interval: 2000
     });
 
     var carousel2 = new bootstrap.Carousel(document.getElementById('carouselExampleFade2'), {
+        interval: 2000
+    });
+
+    var carousel3 = new bootstrap.Carousel(document.getElementById('carouselExampleFade3'), {
         interval: 2000
     });
 }
@@ -11,3 +15,6 @@ function inicializarCarruseles() {
 document.addEventListener('DOMContentLoaded', function() {
     inicializarCarruseles();
 });
+
+
+
